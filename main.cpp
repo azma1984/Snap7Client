1c6aaa74f45771c3942b954e3a62cd0b10888400
SUEKCORP\AzanovAV <123QWE!@^>
2021-11-30 10:55:15 +0700

    Reading all tags at once using ReadArea and extracting the values of the P1-P7, G1, G2, T1-T7, F1-F3, Valve1, Valve2  of tags

--- main.cpp
+++ main.cpp
@@ -45,97 +45,51 @@ int main()
 
 	Client->ReadArea(S7AreaDB, 2, 0, 800, S7WLByte, &result);
 
-	memcpy(&a,&result[20], 4);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP1 = a.f;
-	cout << "fP1 = " << fP1 << endl;
+	a.by[0] = result[23]; a.by[1] = result[22]; a.by[2] = result[21]; a.by[3] = result[20];
+	fP1 = a.f; cout << "fP1 = " << fP1 << endl;
 
+	a.by[0] = result[39]; a.by[1] = result[38]; a.by[2] = result[37]; a.by[3] = result[36];
+	fP2 = a.f; cout << "fP2 = " << fP2 << endl;
+	a.by[0] = result[39]; a.by[1] = result[38]; a.by[2] = result[37]; a.by[3] = result[36];
 
+	a.by[0] = result[55]; a.by[1] = result[54]; a.by[2] = result[53]; a.by[3] = result[52];
+	fP3 = a.f; cout << "fP3 = " << fP3 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 20, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP1 = a.f;
-	cout << "fP1 = " << fP1 << endl;
+	a.by[0] = result[71]; a.by[1] = result[70]; a.by[2] = result[69]; a.by[3] = result[68];
+	fP4 = a.f; cout << "fP4 = " << fP4 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 36, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP2 = a.f;
-	cout << "fP2 = " << fP2 << endl;
+	a.by[0] = result[87]; a.by[1] = result[86]; a.by[2] = result[85]; a.by[3] = result[84];
+	fP5 = a.f; cout << "fP5 = " << fP5 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 52, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP3 = a.f;
-	cout << "fP3 = " << fP1 << endl;
+	a.by[0] = result[103]; a.by[1] = result[102]; a.by[2] = result[101]; a.by[3] = result[100];
+	fP6 = a.f; cout << "fP6 = " << fP6 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 68, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP4 = a.f;
-	cout << "fP4 = " << fP1 << endl;
+	a.by[0] = result[119]; a.by[1] = result[118]; a.by[2] = result[117]; a.by[3] = result[116];
+	fP7 = a.f; cout << "fP7 = " << fP7 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 84, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP5 = a.f;
-	cout << "fP5 = " << fP5 << endl;
+	a.by[0] = result[135]; a.by[1] = result[134]; a.by[2] = result[133]; a.by[3] = result[132];
+	fG1 = a.f; cout << "fG1 = " << fG1 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 100, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP6 = a.f;
-	cout << "fP6 = " << fP6 << endl;
+	a.by[0] = result[151]; a.by[1] = result[150]; a.by[2] = result[149]; a.by[3] = result[148];
+	fG2 = a.f; cout << "fG2 = " << fG2 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 116, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fP7 = a.f;
-	cout << "fP7 = " << fP7 << endl;
+	a.by[0] = result[167]; a.by[1] = result[166]; a.by[2] = result[165]; a.by[3] = result[164];
+	fT1 = a.f; cout << "fT1 = " << fT1 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 132, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fG1 = a.f;
-	cout << "fG1 = " << fG1 << endl;
+	a.by[0] = result[183]; a.by[1] = result[182]; a.by[2] = result[181]; a.by[3] = result[180];
+	fT2 = a.f; cout << "fT2 = " << fT2 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 148, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fG2 = a.f;
-	cout << "fG2 = " << fG2 << endl;
+	a.by[0] = result[199]; a.by[1] = result[198]; a.by[2] = result[197]; a.by[3] = result[196];
+	fT3 = a.f; cout << "fT3 = " << fT3 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 164, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fT1 = a.f;
-	cout << "fT1 = " << fT1 << endl;
+	a.by[0] = result[215]; a.by[1] = result[214]; a.by[2] = result[213]; a.by[3] = result[212];
+	fT4 = a.f; cout << "fT4 = " << fT4 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 180, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fT2 = a.f;
-	cout << "fT2 = " << fT2 << endl;
+	a.by[0] = result[231]; a.by[1] = result[230]; a.by[2] = result[229]; a.by[3] = result[228];
+	fT5 = a.f; cout << "fT5 = " << fT5 << endl;
 
-	Client->ReadArea(S7AreaDB, 2, 196, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fT3 = a.f;
-	cout << "fT3 = " << fT3 << endl;
-
-	Client->ReadArea(S7AreaDB, 2, 212, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fT4 = a.f;
-	cout << "fT4 = " << fT4 << endl;
-
-	Client->ReadArea(S7AreaDB, 2, 228, S7WLReal, 4, &a);
-	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
-	by1 = a.by[1]; a.by[1] = a.by[2]; a.by[2] = by1;
-	fT5 = a.f;
-	cout << "fT5 = " << fT5 << endl;
+	a.by[0] = result[247]; a.by[1] = result[246]; a.by[2] = result[245]; a.by[3] = result[244];
+	fT6 = a.f; cout << "fT6 = " << fT6 << endl;
 
 	Client->ReadArea(S7AreaDB, 2, 244, S7WLReal, 4, &a);
 	by1 = a.by[0]; a.by[0] = a.by[3]; a.by[3] = by1;
